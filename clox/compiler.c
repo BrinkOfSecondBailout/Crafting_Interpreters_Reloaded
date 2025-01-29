e41b499b14546b9739333b4e36ee4b7d66cac922
Danny Nguyen <nguyenduongpa@gmail.com>
2025-01-29 11:45:07 -0800

    global variables are done

--- clox/compiler.c
+++ clox/compiler.c
@@ -32,7 +32,7 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
-typedef void (*ParseFn)();
+typedef void (*ParseFn)(bool canAssign);
 
 typedef struct {
     ParseFn prefix;
@@ -145,7 +145,7 @@ static void declaration();
 static ParseRule *getRule(TokenType type);
 static void parsePrecedence(Precedence precedence);
 
-static void binary() {
+static void binary(bool canAssign) {
     // Save the operator type of the previously consumed token (e.g., +, -, *, /).
     TokenType operatorType = parser.previous.type;
 
@@ -176,7 +176,8 @@ static void binary() {
     }
 }
 
-static void literal() {
+static void literal(bool canAssign)
+{
     switch (parser.previous.type) {
         case TOKEN_FALSE: emitByte(OP_FALSE); break;
         case TOKEN_NIL: emitByte(OP_NIL); break;
@@ -185,30 +186,40 @@ static void literal() {
     }
 }
 
-static void grouping() {
+static void grouping(bool canAssign)
+{
     expression();
     consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
 }
 
-static void number() {
+static void number(bool canAssign)
+{
     double value = strtod(parser.previous.start, NULL);
     emitConstant(NUMBER_VAL(value));
 }
 
-static void string() {
+static void string(bool canAssign)
+{
     emitConstant(OBJ_VAL(copyString(parser.previous.start + 1, parser.previous.length - 2)));
 }
 
-static void namedVariable(Token name) {
+static void namedVariable(Token name, bool canAssign) {
     uint8_t arg = identifierConstant(&name);
-    emitBytes(OP_GET_GLOBAL, arg);
+
+    if (canAssign && match(TOKEN_EQUAL)) {
+        expression();
+        emitBytes(OP_SET_GLOBAL, arg);
+    } else {
+        emitBytes(OP_GET_GLOBAL, arg);
+    }
 }
 
-static void variable() {
-    namedVariable(parser.previous);
+static void variable(bool canAssign) {
+    namedVariable(parser.previous, canAssign);
 }
 
-static void unary() {
+static void unary(bool canAssign)
+{
     TokenType operatorType = parser.previous.type;
     parsePrecedence(PREC_UNARY);
     switch (operatorType) {
@@ -275,7 +286,8 @@ static void parsePrecedence(Precedence precedence) {
     }
 
     // Call the prefix parsing function. This handles things like numbers, unary operators, or parentheses.
-    prefixRule();
+    bool canAssign = precedence <= PREC_ASSIGNMENT;
+    prefixRule(canAssign);
 
     // Now process any infix operators while their precedence is greater than or equal to the current precedence.
     while (precedence <= getRule(parser.current.type)->precedence) {
@@ -286,7 +298,11 @@ static void parsePrecedence(Precedence precedence) {
         // Call the infix parsing function. This will:
         // - Handle the current operator.
         // - Parse the operator's right-hand operand recursively with higher precedence.
-        infixRule();
+        infixRule(canAssign);
+
+        if (canAssign && match(TOKEN_EQUAL)) {
+            error("Invalid assignment target.");
+        }
         // Once no more valid infix operators are found, the function exits.
     }
 }
